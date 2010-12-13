6c4401a33233ed1be2dc5012dd7ca6f43e58e76d
Ryan Wolf <rwolf@borderstylo.com>
2010-12-13 13:17:33 -0800

    cut out the middleman, drive the browser from inside the iframe

--- public/javascripts/runner.js
+++ public/javascripts/runner.js
@@ -1,39 +1,24 @@
-// handle communication with the browser
-var Browser = function () {
-  var that = this;
-  this.requestHandler = function () {};
-  window.addEventListener(
-    'message',
-    function (e) { that.readMessage(e.data); },
-    false
-  );
-};
-Browser.prototype.sendMessage = function (json) {
-  window.parent.postMessage(json, '*');
-};
-Browser.prototype.readMessage = function (json) {
-  this.requestHandler(json);
-};
+netscape.security.PrivilegeManager.enablePrivilege('UniversalXPConnect');
 
 // handle communication with the server
-var Server = function () {
+var Server = function (requestHandler) {
   var that = this;
-  this.requestHandler = function () {};
+  this.requestHandler = requestHandler;
   var socket = this.socket = new io.Socket('localhost', {
     transports: ['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling']
   });
   socket.connect();
   socket.on('message', function (data) { that.readMessage(data); });
 };
-Server.prototype.sendMessage = function (json) {
+Server.prototype.sendMessage = function (obj) {
+  var json = JSON.stringify(obj);
   this.socket.send(json);
 };
 Server.prototype.readMessage = function (json) {
-  this.requestHandler(json);
+  var obj = JSON.parse(json);
+  this.requestHandler(obj);
 };
 
-var browser = new Browser();
-var server = new Server();
-
-browser.requestHandler(server.sendMessage);
-server.requestHandler(browser.sendMessage);
+var server = new Server(function (request) {
+  alert(JSON.stringify(request));
+});
